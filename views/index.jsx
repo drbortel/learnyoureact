79182d9ce5ed33ae65f043871bd79364c9a5f9be
Darren Bortel <drbortel@gmail.com>
2016-02-17 20:36:30 -0600

    updates to index.jsx

--- views/index.jsx
+++ views/index.jsx
@@ -1,22 +1,42 @@
- import React from 'react';
+import React from 'react';
 
-    export default class TodoBox extends React.Component {
-        render() {
-            return (
-                <div className="todoBox">
-                    <h1>Todos</h1>
-                    <TodoList />
-                    <TodoForm />
-                </div>
-            );
-        }
-    }
+export default class TodoBox extends React.Component {
+ // Omitted
+}
+
+class TodoList extends React.Component {
+   render() {
+       return (
+           <div className="todoList">
+               <table style={{border: "2px solid black"}}>
+                   <tbody>
+                   <Todo title="Shopping">Milk</Todo>
+                   <Todo title="Hair cut">13:00</Todo>
+                   </tbody>
+               </table>
+           </div>
+       );
+   }
+}
 
     class TodoList extends React.Component {
         render() {
-            return <div className="todoList">
-                I am a TodoList.
-            </div>;
+            return (
+              <div class="todoList">
+                <table style="border:2px solid black;">
+                  <tbody>
+                    <tr>
+                      <td style="border:1px solid black;">Shopping</td>
+                      <td style="border:1px solid black;">Milk</td>
+                    </tr>
+                    <tr>
+                      <td style="border:1px solid black;">Hair cut</td>
+                      <td style="border:1px solid black;">13:00</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            );
         }
     }
 
